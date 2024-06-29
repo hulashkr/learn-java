94ac81615f473df0754ad7e8a42a19f9ea67611f
Hulash Kumar <hulashkrcps@gmail.com>
2024-06-29 14:05:29 +0530

    add another file

---
+++ CheckRangeAndSize.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class CheckRangeAndSize
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Double min : " + Double.MIN_VALUE);
+        System.out.println("Double max : " + Double.MAX_VALUE);
+        System.out.println("Size in bytes : " + Double.BYTES);
+    }
+}
