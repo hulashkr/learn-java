6411fc6b4d3539eee4c3cbe0e930937bbccfd409
Hulash Kumar <hulashkrcps@gmail.com>
2024-07-11 15:30:52 +0530

    add Literals.java

---
+++ DeclarationAndInitialization.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class DeclarationAndInitialization
+{
+    public static void main(String[] args)
+    {
+        // int a;
+        // System.out.println(a); // this will give an error as variable "a" is only declared and not initialized
+
+        int b = 5;
+        System.out.println(b); // 5
+        b = 12;
+        System.out.println(b); // 12
+    }
+}
---
+++ Literals.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class Literals
+{
+    public static void main(String[] args)
+    {
+        byte a = 10;
+        byte b = 0b1010;
+        byte c = 012;
+        byte d = 0xA;
+
+        System.out.println(a); // 10
+        System.out.println(b); // 10
+        System.out.println(c); // 10
+        System.out.println(d); // 10
+    }
+}
