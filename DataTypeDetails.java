6571faf3d64b4b629484006ccb3444482a018ef9
Hulash Kumar <hulashkrcps@gmail.com>
2024-07-11 16:11:12 +0530

    add DataTypeDetails.java

---
+++ DataTypeDetails.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class DataTypeDetails
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = -5;
+
+        // How the numbers are stored in binary form
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+    }
+}
